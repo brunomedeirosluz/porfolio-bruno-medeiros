69ed23586bf6d9fe43be90787c8e6cce4cbc0770
brunomedeirosluz <brunoml95@hotmail.com>
2024-10-21 19:58:09 -0300

    feat: fazendo home, footer e menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-
+  isMenuActive: boolean = false;
+  ativar() {
+    const toggle = document.getElementById('toggle');
+    toggle?.classList.toggle('active');
+    this.isMenuActive = !this.isMenuActive;
+  }
 }
